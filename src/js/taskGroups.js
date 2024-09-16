2dd6ad9bc15a43e5aa4f5daebd8238e84ba5b0fe
Tymoteusz Huba <tymoteuszhuba@MacBook-Air-Tymoteusz.local>
2024-09-16 20:54:36 +0200

    added another section groups and styles for that section

--- src/js/taskGroups.js
+++ src/js/taskGroups.js
@@ -3,14 +3,40 @@ import {createElement, createElements, addSeveral} from './functions';
 const main = document.querySelector('main');
 
 let taskGroupElements = [
-  {
-    tag: 'button',
-    className: 'task-group-add',
-    content: '+',
-  },
-  {
-    tag: 'button',
-    className: 'task-group-home',
-    content: 'Home',
-  } 
+	{
+		tag: 'button',
+		className: 'groups-btn groups-add-btn',
+		content: `<i class="fa-regular fa-plus"></i>`,
+	},
+	{
+		tag: 'button',
+		className: 'groups-btn groups-home',
+		content: 'Home',
+	},
+	{
+		tag: 'button',
+		className: 'groups-btn groups-work',
+		content: 'Work',
+	},
+	{
+		tag: 'button',
+		className: 'groups-btn groups-shipping',
+		content: 'Shop',
+	},
+	{
+		tag: 'button',
+		className: 'groups-btn groups-shipping',
+		content: 'Gym',
+	},
 ];
+
+const createGroupElements = () => {
+	const groupContainer = createElement('section', {className: 'groups'});
+
+	const groupsContent = createElements(taskGroupElements);
+
+	addSeveral(groupsContent, groupContainer);
+	main.appendChild(groupContainer);
+};
+
+export {createGroupElements};
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import './css/main.css';
 import {handleScreenChange} from './js/matchMedia';
 import {createHeaderElements} from './js/header';
 import {createCalendarElements} from './js/calendar';
+import {createGroupElements} from './js/taskGroups'
 
 // INITIAL
 function initialize() {
 	createHeaderElements();
 	createCalendarElements();
+	createGroupElements();
 }
 
 //initialize();
